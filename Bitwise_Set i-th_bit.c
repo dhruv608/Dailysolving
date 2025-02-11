5d283979a382fc94d96c7716677fd0aa33ab8f91
dhruv608 <dhruvnarang608@gmail.com>
2025-02-11 10:39:18 +0530

    ques19

---
+++ Bitwise_Set i-th_bit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+int n;
+    int i;
+    scanf("%d\n%d",&n,&i);
+    n=(n|(1<<i));
+    printf("%d",n);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
