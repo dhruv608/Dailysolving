b3e8930448bc475226ee287cea258ef32bb47309
dhruv608 <dhruvnarang608@gmail.com>
2025-02-13 23:05:36 +0530

    ques21

--- Bitwise_Toggle_i-th_bit.c
+++ Bitwise_Toggle_i-th_bit.c
@@ -4,7 +4,8 @@ int main() {
 int n;
     int i;
     scanf("%d\n%d",&n,&i);
-    n=(n&(~(1<<i)));
+    n=(n^(1<<i));
     printf("%d",n);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
